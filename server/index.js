2bd5ccb0c4ea78df32e81dc2df2451b14d8f694a
Izzatbek <abdusharipovizzat03@gmail.com>
2025-10-23 09:59:23 +0500

    movie time

--- server/index.js
+++ server/index.js
@@ -6,6 +6,10 @@ const cors=require("cors");
 const app=express();
 
 app.use(express.json());
+app.use(cors({
+    credentials:true,
+    origin:process.env.CLIENT
+}))
 
 const port=process.env.PORT || 5000;
 
