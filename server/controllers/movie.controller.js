518a372552dce3e106223a8d67d889a7b256dda4
Izzatbek <abdusharipovizzat03@gmail.com>
2025-10-23 18:59:47 +0500

    ready navbar section

--- server/controllers/movie.controller.js
+++ server/controllers/movie.controller.js
@@ -2,5 +2,14 @@ const movieService=require("../services/movie.service");
 
 class MovieController{
 
+    async (req,res){
+        try {
+            
+        } catch (error) {
+            throw new Error("Internal Server Error");
+            
+        }
+    }
+
 }
 module.exports=new MovieController();
--- server/services/movie.service.js
+++ server/services/movie.service.js
@@ -2,5 +2,18 @@ const axios = require("axios");
 
 class MovieService{
 
+    // popular,top_rated,upcoming,trending,now_playing
+    async getMovies(category){
+
+    }
+
+    async getTVList(){
+
+    }
+
+    async getCartoons(){
+        
+    }
+
 }
 module.exports=new MovieService();
