2e53ae242945c76b55c2a3fd0d84f557634a58d9
Izzatbek <abdusharipovizzat03@gmail.com>
2025-10-24 12:24:55 +0500

    movie time

---
+++ server/dto/Movie.js
@@ -0,0 +1,19 @@
+module.exports=class Movie{
+    id;
+    backdrop_path;
+    genre_ids;
+    title;
+    overview;
+    poster_path;
+    release_date
+
+    constructor(movie){
+        this.id=movie.id;
+        this.backdrop_path=movie.backdrop_path;
+        this.genre_ids=movie.genre_ids;
+        this.title=movie.title;
+        this.overview=movie.overview;
+        this.poster_path=movie.poster_path;
+        this.release_date=movie.release_date;
+    }
+}
