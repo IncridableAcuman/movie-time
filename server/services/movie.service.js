f022ce48557435e326ff539528921f9c7a5e94a8
Izzatbek <abdusharipovizzat03@gmail.com>
2025-10-24 12:50:42 +0500

    movie time

--- server/services/movie.service.js
+++ server/services/movie.service.js
@@ -1,5 +1,5 @@
 const axios = require("axios");
-
+const Movie=require("../dto/Movie");
 const url=process.env.TMDB_URL || "12";
 const key=process.env.KEY || "aa";
 class MovieService{
@@ -8,25 +8,27 @@ class MovieService{
     // popular,top_rated,upcoming,now_playing
     async getMovies(category){
         const {data} = await axios.get(`${url}/movie/${category}?api_key=${key}&language=en-US`);
-        return data;
+        return data.results.map(m=>new Movie(m));
     }
 
     //popular,top_rated,on_the_air
     async getTVList(category){
         const {data} = await axios.get(`${url}/tv/${category}?api_key=${key}&language=en-US`);
-        return data;
+        return data.results.map(m=>new Movie(m));
+
     }
 
     async getCartoons(){
         const {data} = await axios.get(`${url}/discover/movie?api_key=${key}&with_genres=16&language=en-US`);
-        return data;
-        console.log(data)
+        return data.results.map(m=>new Movie(m));
+
     }
 
     // movie,tv
     async getGeneres(category){
         const {data} = await axios.get(`${url}/genre/${category}/list?api_key=${key}&language=en`);
-        return data;
+        return data.results.map(m=>new Movie(m));
+
     }
 
 }
