d90b6e19d170401499b99ee5ea9d56a5bac23da6
Izzatbek <abdusharipovizzat03@gmail.com>
2025-10-27 22:25:04 +0500

    movie time

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const movieRoute=require("./routes/movie.routes");
 const app=express();
 
 app.use(express.json());
-app.use(cors({credentials:true,origin:process.env.CLIENT}));
+app.use(cors({credentials:true,origin:"http://localhost:3000"}));
 
 app.use("/api",movieRoute);
 
