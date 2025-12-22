3252de2058c4d0ecce9f7c2a898ea89c2d6d0254
Izzatbek <abdusharipovizzat03@gmail.com>
2025-12-22 15:32:16 +0500

    authentication in progress

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,6 +1,5 @@
 import { Outlet } from "react-router-dom"
 import Sidebar from "../components/Sidebar"
-import Navbar from "../components/Navbar"
 
 const Layout = () => {
   return (
@@ -8,8 +7,7 @@ const Layout = () => {
       <aside className={`hidden md:block  w-64 h-screen shadow-2xl bg-black/40 text-white overflow-y-auto`}>
         <Sidebar/>
       </aside>
-      <main className="flex-1 min-h-screen inset-0 overflow-y-auto p-4">
-        <Navbar/>
+      <main className="flex-1 min-h-screen inset-0 overflow-y-auto p-4 shrink-0">
         <Outlet/>
       </main>
     </div>
