db1a4213a09742a85efda19ded9b06785b451d2b
Izzatbek <abdusharipovizzat03@gmail.com>
2025-12-22 17:24:54 +0500

    in progress

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,8 +3,9 @@ const Home = () => {
   return (
     <>
       <div className="w-full min-h-screen">
-        <div className="pt-24 px-8">
-          <div className="flex items-center overflow-x-auto gap-2">
+        <div className="p-8">
+          <img src="./bnr.jfif" alt="banner" className="w-full h-96 object-cover rounded-lg shadow-2xl shadow-pink-600" />
+          <div className="flex items-center overflow-x-auto gap-2 pt-16">
 
             <div className="min-w-96 max-w-96 bg-gray-900 text-white border border-gray-800 shadow-md hover:shadow-2xl hover:shadow-pink-600 transition duration-300 p-4">
               <img src="./image.png" alt="logo" className="rounded-t-md cursor-pointer" />
