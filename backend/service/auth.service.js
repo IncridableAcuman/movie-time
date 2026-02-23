5181cb7d82c918f24d7d5e4af9b8c60eab314f49
Izzatbek <abdusharipovizzat03@gmail.com>
2026-02-23 21:24:59 +0500

    Movie Time

--- backend/service/auth.service.js
+++ backend/service/auth.service.js
@@ -1,4 +1,23 @@
  class AuthService{
 
+    async register(username,email,password){
+
+    }
+    async login(email,password){
+
+    }
+    async refresh(refreshToken){
+
+    }
+    async logout(refreshToken){
+
+    }
+    async forgotPassword(email){
+
+    }
+    async resetPassword(token,password){
+        
+    }
+
 }
-module.exports=AuthService;
+module.exports = new AuthService();
--- backend/service/token.service.js
+++ backend/service/token.service.js
@@ -1,14 +1,41 @@
-const { Schema, model } = require('mongoose');
+const jwt = require('jsonwebtoken')
+const Token = require('../model/token.model')
+class TokenService {
 
-const TokenSchema = new Schema({
-    refreshToken: {
-        type: String,
-        required: true
-    },
-    user: {
-        type: Schema.Types.ObjectId,
-        ref: 'User'
+    generateToken(payload) {
+        const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH, { expiresIn: '30d' });
+        const accessToken = jwt.sign(payload, process.env.JWR_ACCESS, { expiresIn: '15m' })
+        return { accessToken, refreshToken };
     }
-});
-const Token = model('Token', TokenSchema);
-export default Token;
+
+    async saveToken(userId, refreshToken) {
+        const user = await Token.findOne({ user: userId });
+        if (user) {
+            user.refreshToken = refreshToken;
+            return user.save();
+        }
+        const token = await Token.create({ user: userId, refreshToken });
+        return token;
+    }
+    async findToken(refreshToken){
+        return await Token.findOne({refreshToken});
+    }
+    async removeToken(refreshToken){
+        return await Token.findOneAndDelete({refreshToken});
+    }
+    validateAccessToken(accessToken){
+        try {
+            return jwt.verify(accessToken,process.env.JWR_ACCESS);
+        } catch (error) {
+            return null;
+        }
+    }
+    validateRefreshToken(refreshToken){
+        try {
+            return jwt.verify(refreshToken,process.env.JWT_REFRESH);
+        } catch (error) {
+            return null;
+        }
+    }
+}
+module.exports = new TokenService();
