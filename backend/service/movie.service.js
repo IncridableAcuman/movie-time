8cc3ccb71cec95ee762782772460627feb9741ce
Izzatbek <abdusharipovizzat03@gmail.com>
2026-02-25 21:16:31 +0500

    Movie Time

--- backend/service/movie.service.js
+++ backend/service/movie.service.js
@@ -1,22 +1,23 @@
 const axios = require('axios')
-const BaseError=require('../error/base.error')
+const BaseError = require('../error/base.error')
 const Dto = require("../dto/movie.dto")
 
 
 class MovieService {
 
     async movieList(category) {
-        if(!category){
+        if (!category) {
             throw BaseError.BadRequest("Category must be required!")
         }
-        const {data} = await axios.get(`https://api.themoviedb.org/3/movie/${category}`,{
-            params:{
-                api_key:process.env.TMDB_KEY,
+        const { data } = await axios.get(`https://api.themoviedb.org/3/movie/${category}`, {
+            params: {
+                api_key: process.env.TMDB_KEY,
                 language: 'en-US',
-                page:1
+                page: 1
             }
         })
-        return data.results.map((movie)=> new Dto(movie))
+        const results = data?.results || [];
+        return results.map((movie) => new Dto(movie));
 
     }
 
